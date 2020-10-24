d0d77b0f1e18d8bd3f3774127d2cbef3893ab676
cfar0059 <cfar0059@gmail.com>
2020-10-24 18:32:30 +0200

    Created GithubState, Reducer and Context BoilerPlate.

---
+++ src/context/types.js
@@ -0,0 +1,10 @@
+//types are variables of string that you can use
+//to change your state within your reducer
+
+export const SEARCH_USERS = 'SEARCH_USERS';
+export const GET_USERS = 'GET_USERS';
+export const CLEAR_USERS = 'CLEAR_USERS';
+export const GET_REPOS = 'GET_REPOS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
